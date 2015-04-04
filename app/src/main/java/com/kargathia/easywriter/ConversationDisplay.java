aae3fadf7ba5114f7e41e018cad443482fe008bf
Kargathia <kargathia@hotmail.com>
2015-04-04 21:06:34 +0200

    implemented swipetest

--- app/src/main/java/com/kargathia/easywriter/ConversationDisplay.java
+++ app/src/main/java/com/kargathia/easywriter/ConversationDisplay.java
@@ -1,18 +1,35 @@
 package com.kargathia.easywriter;
 
 import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 
 public class ConversationDisplay extends Activity {
+
+    private RelativeLayout layoutHistory;
+    private EditText tfTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversation_display);
+
+        ActivitySwipeDetector activitySwipeDetector = new ActivitySwipeDetector(this);
+        layoutHistory = (RelativeLayout) this.findViewById(R.id.layoutHistory);
+        layoutHistory.setOnTouchListener(activitySwipeDetector);
+
+        this.tfTest = (EditText) this.findViewById(R.id.tfTestField);
     }
 
 
@@ -38,19 +55,38 @@ public class ConversationDisplay extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void startWriting(){
+    private void setOnClicks() {
 
     }
 
-    public void letterOK(){
+    public void startWriting() {
 
     }
 
-    public void backGesture(){
+    public void letterOK() {
 
     }
 
-    public void acceptGesture(){
+    public void backGesture() {
+        int length = tfTest.getText().length();
+        if (length > 0) {
+//            tfTest.getText().delete(length - 1, length);
+            tfTest.dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DEL));
+            tfTest.dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_DEL));
+        } else {
+            displayToast("going back now");
+        }
+    }
 
+    public void acceptGesture() {
+        displayToast("sending message: " + tfTest.getText().toString());
+    }
+
+    private void displayToast(String text) {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
